9b4a63ad075b09f4e3bed85ab810b631a41bd08b
Stefano Davi Almeida Silva <sdavi-al@c1r2p1.42sp.org.br>
2025-06-17 15:27:13 -0300

    readline and add_history

---
+++ Makefile
@@ -0,0 +1,31 @@
+NAME = minishell
+
+SRC =	main.c \
+
+
+OBJ = $(SRC:.c=.o)
+
+CC = cc
+
+CFLAGS = -Wall -Wextra -Werror
+
+LIBS = -lreadline
+
+
+all: $(NAME)
+
+$(NAME): $(OBJ)
+	$(CC) $(CFLAGS) $(OBJ) -o $(NAME) $(LIBS)
+
+%.o: %.c
+	$(CC) $(CFLAGS) -c $< -o $@
+
+clean:
+	rm -f $(OBJ)
+
+fclean: clean
+	rm -f $(NAME)
+
+re: fclean all
+
+.PHONY: all clean fclean re
---
+++ main.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sdavi-al <sdavi-al@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/17 14:33:20 by sdavi-al          #+#    #+#             */
+/*   Updated: 2025/06/17 14:51:30 by sdavi-al         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+
+int	main(void)
+{
+	char	*line;
+
+	while (1)
+	{
+		line = readline("minishell> ");
+		if (line == NULL)
+		{
+			printf("exit\n");
+			break ;
+		}
+		if (line[0] != '\0')
+		{
+			add_history(line);
+		}
+		if (strcmp(line, "exit") == 0)
+		{
+			free(line);
+			break ;
+		}
+		printf("You entered: %s\n", line);
+		free(line);
+	}
+	return (0);
+}
